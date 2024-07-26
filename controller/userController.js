2b7205c9b20f4fe2de5f02bd240efb129f67f6c0
git-groot <github437@gmail.com>
2024-07-26 11:56:29 +0530

    login code

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 
+const { json } = require('body-parser');
 const userModule = require('../modules/userModule');
+const { use } = require('../routes/userRoute');
 
 
 
@@ -21,7 +23,38 @@ exports.getallUser = async (req, res) => {
         const alluser = await userModule.find();
         res.status(200).json(alluser);
     }
-    catch(error){
+    catch (error) {
         res.status(500).json(error)
     }
-}
+},
+
+    exports.getsingle = async (req, res) => {
+        const user = await userModule.findById(req.params.id);
+        try {
+            if (!user) {
+                return res.status(404).json({ mess: 'invalide user id' });
+            }
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json(error);
+        };
+
+    },
+
+    exports.loginUser = async (req, res) => {
+        const { email, mobile, password } = req.body;
+        try {
+            const user = await userModule.findOne({ $or: [{ email }, { mobile }] });
+            // res.json(user);
+            if (!user) {
+                return res.status(404).json({ mes: 'invalide email or mobile' });
+            }
+            if (userModule.password !== password) {
+                 res(404).json({ mes: "incorrect password" });
+            }
+            res.json(user);
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,11 @@ const route = express.Router();
 
 route.post('/addUser', userControl.adduser);
 
-route.get('/getall',userControl.getallUser)
+route.get('/getall',userControl.getallUser);
+
+route.get('/user/:id',userControl.getsingle);
+
+route.post('/user/login',userControl.loginUser);
 
 module.exports = route;
 
